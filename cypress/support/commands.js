9f88f3c1dbc80e663544972e1c05725590843373
TrojnarPawel <63153575+TrojnarPawel@users.noreply.github.com>
2022-07-19 12:49:14 +0200

    testylogowania (#1)
    * testylogowania
    
    * after-loging-cookies

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,12 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+function gennerateTestSelector (value) {
+    return '[data-test=${value}]';
+}
+
+Cypress.Commands.add("getSelector", (value) => { 
+    return cy.get(generateTestSelector(value))
+});
+
